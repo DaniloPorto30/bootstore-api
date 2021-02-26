84798a16062fa3cf6598a578c6028936db13abfd
DaniloPorto30 <dprosa23@gmail.com>
2021-02-26 10:58:11 -0300

    commit inicial

---
+++ src/main/java/com/danilo/bootstore/BootstoreApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.bootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstoreApplication.class, args);
+	}
+
+}
